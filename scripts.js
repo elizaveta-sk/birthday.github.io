40186162549185526208bf3d80c51ae790e4460b
elizaveta-sk <147830613+elizaveta-sk@users.noreply.github.com>
2024-07-02 23:23:31 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,11 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
             giftOptions.classList.add('hidden');
             finalStep.classList.remove('hidden');
             if (index === 0) {
-                box.innerHTML = '<iframe width="150" height="150" src="https://www.youtube.com/embed/op0aJGB165A" frameborder="0" allowfullscreen></iframe>';
+                box.innerHTML = '<iframe width="640" height="360" src="https://www.youtube.com/embed/op0aJGB165A" frameborder="0" allowfullscreen></iframe>';
             } else if (index === 1) {
-                box.innerHTML = '<iframe width="150" height="150" src="https://www.youtube.com/embed/ufit4zSvTJ0" frameborder="0" allowfullscreen></iframe>';
+                box.innerHTML = '<iframe width="640" height="360" src="https://www.youtube.com/embed/ufit4zSvTJ0" frameborder="0" allowfullscreen></iframe>';
             } else if (index === 2) {
-                box.innerHTML = '<iframe width="150" height="150" src="https://www.youtube.com/embed/3AL0I-V88Do" frameborder="0" allowfullscreen></iframe>';
+                box.innerHTML = '<iframe width="640" height="360" src="https://www.youtube.com/embed/3AL0I-V88Do" frameborder="0" allowfullscreen></iframe>';
             } else if (index === 3) {
                 box.innerHTML = '<a href="https://www.klook.com/en-US/activity/41323-tokyo-joypolis/" target="_blank">Link to activity</a>';
             }
